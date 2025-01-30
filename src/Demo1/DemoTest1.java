36e555b072c8baa3fe2c1a0ca940364397319e7a
Jayme Morillo <emipookie07@gmail.com>
2025-01-29 19:23:37 -0700

    Update DemoTest1.java
    add changes

--- src/Demo1/DemoTest1.java
+++ src/Demo1/DemoTest1.java
@@ -4,6 +4,12 @@ public class DemoTest1 {
 	
 	public static void main (String args []) {
 		System.out.println("Jayme");
+
+		int a = 10;
+		int b = 20;
+		int result = a+b;
+
+		System.out.println(result);
 }
 	
 	public void add (int a, int b) {
